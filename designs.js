a8703941e40fe52e31b37aea404ca698edbc2630
rluftw <luxinghui@johndeere.com>
2018-02-05 21:15:06 -0600

    add assignment solution

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
-// Select color input
-// Select size input
+// event listeners
 
-// When size is submitted by the user, call makeGrid()
+$('input[type=submit]').on('click', function( event ){
+	event.preventDefault();
+	makeGrid();
+})
+
+$('#pixelCanvas').on('click', 'td', function( event ) {
+	color = $('input[type=color]').val()
+	$(this).css('background-color', color);
+})
+
+// functions
 
 function makeGrid() {
+	// reset the grid
+	$('#pixelCanvas').empty();
 
-// Your code goes here!
+	const height = $('input[type=number]').val();
+	const width = $('input[type=number]').next().val();
 
+	for(var rowCount=0; rowCount<height; rowCount++) {
+		createRow(width);
+	}
+}
+
+function createRow(width) {
+	$('#pixelCanvas').append('<tr></tr>')
+	for(var columnCount=0; columnCount<width; columnCount++) {
+		$('tr:last-of-type').append('<td></td>')
+	}
 }
